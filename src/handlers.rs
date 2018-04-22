cc1d181352852966466606dfbedf0493bd43da6a
Rrogntudju <mario.prince@outlook.com>
2018-04-22 15:02:44 -0400

    response

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,7 +3,7 @@ pub mod api {
 
     use gotham::http::response::create_response;
     use gotham::state::{State, FromState};
-    use gotham::handler::{HandlerFuture, HandlerError};
+    use gotham::handler::HandlerFuture;
     use hyper::{Body, Chunk, Error, Response, StatusCode};
     use mime;
     use futures::{Future, future, Stream};
@@ -11,30 +11,25 @@ pub mod api {
     use sudoku::{Sudoku, PuzzleError};
     
 
-    #[derive(Serialize, Deserialize)]
-    struct SudokuPuzzle<T> {
-        puzzle: T
+    #[derive(Deserialize)]
+    struct SudokuRequest {
+        puzzle: String
     }
     
     #[derive(Serialize)]
-    struct SudokuMessage {
-        message: String
+    struct SudokuResponse<T> {
+        status: String,
+        data: T
     }    
 
-    type SolutionFuture = Future<Item=String, Error=PuzzleError>;
+    #[derive(Serialize)]
+    struct DisplayData {
+        display: Vec<String>
+    }
 
-    fn solve_sudoku2<F>(fut: F) -> impl Future<Item=String, Error=PuzzleError> 
-        where F: Future<Item=String, Error=PuzzleError> {
-            fut.then(move |result| { 
-                    let solver_result = result.and_then(|puzzle| {
-                        let sdk = Sudoku::new();
-                        sdk.solve(&puzzle)
-                    });
-                    match solver_result {
-                        Ok(solution) => future::ok(solution),
-                        Err(e) => future::err(e)
-                    }
-            })
+    #[derive(Serialize)]
+    struct SolutionData {
+        solution: String
     }
 
     fn solve_sudoku(body: Result<Chunk, Error>) -> impl Future<Item=String, Error=PuzzleError> {
@@ -42,7 +37,7 @@ pub mod api {
             Ok(valid_body) => {
                 let body_content = String::from_utf8(valid_body.to_vec()).unwrap();
                 println!("Body: {}", body_content);
-                let sudoku_puzzle: Result<SudokuPuzzle<String>, _> = serde_json::from_str(&body_content);
+                let sudoku_puzzle: Result<SudokuRequest, _> = serde_json::from_str(&body_content);
                 match sudoku_puzzle {
                     Ok(sp) => {
                         let solution = Sudoku::new().solve(&sp.puzzle);
@@ -57,70 +52,28 @@ pub mod api {
             _ => future::err(PuzzleError::InvalidGrid)
         }
     }
-/* 
-    fn solve_sudoku<F: 'static>(fut: F) -> Box<Future<Item=String, Error=PuzzleError>> 
-        where F: Future<Item=String, Error=PuzzleError> {
-            let f =
-                fut.then(move |result| { 
-                    let solver_result = result.and_then(|puzzle| {
-                        let sdk = Sudoku::new();
-                        sdk.solve(&puzzle)
-                    });
-                    match solver_result {
-                        Ok(solution) => future::ok(solution),
-                        Err(e) => future::err(e)
-                    }
-                });
-            Box::new(f)
-    } */
 
-    pub fn solve(mut state: State) -> impl Future<Item = (State, Response), Error = (State, HandlerError)> {
-        
+    pub fn solve(mut state: State) -> Box<HandlerFuture> {
         let fut = 
             Body::take_from(&mut state)
             .concat2()
-            .then(solve_sudoku);
-/*             .then(|full_body| {
-                let future_solution: SolutionFuture = {
-                    let x =
-                    match full_body {
-                        Ok(valid_body) => {
-                            let body_content = String::from_utf8(valid_body.to_vec()).unwrap();
-                            println!("Body: {}", body_content);
-                            let sudoku_puzzle: Result<SudokuPuzzle<String>, _> = serde_json::from_str(&body_content);
-                            match sudoku_puzzle {
-                                Ok(sp) => {
-                                    let solution = Sudoku::new().solve(&sp.puzzle);
-                                    match solution {
-                                        Ok(s) => future::ok(s),
-                                        Err(e) => future::err(e)
-                                    }
-                                }
-                                _ => future::err(PuzzleError::InvalidGrid)
-                            }
-                        }
-                        _ => future::err(PuzzleError::InvalidGrid)
+            .then(solve_sudoku)
+            .then(|solve_result| { 
+                let sudoku_response = 
+                    match solve_result {
+                        Ok(solution) => SudokuResponse {status: "success".into(), data: SolutionData {solution: solution}},
+                        Err(e) => SudokuResponse {status: "fail".into(), data: SolutionData {solution: format!("{}", e)}},
                     };
- //                   Box::new(x)
-                };
-                future_solution    
-            });
-        //     .then( */ //|solve_result| { match solve_result {
-        //         Ok(solution) => {
-        //             let sudoku_solution = SudokuPuzzle {puzzle: solution};
-        //             let resp = create_response(
-        //                 &state,
-        //                 StatusCode::Ok,
-        //                 Some((Vec<u8>::From(serde_json::to_string(&sudoku_solution).unwrap()), mime::APPLICATION_JSON))
-        //             );
-        //             future::ok((state, resp))
-        //         }
-        //         Err(e) => 
-        //     }
-        // });
+                let resp = create_response(
+                    &state,
+                    StatusCode::Ok,
+                    Some((serde_json::to_string(&sudoku_response).unwrap().into_bytes(), mime::APPLICATION_JSON))
+                );
+                future::ok((state, resp))
+            }
+        );
 
-    future::ok()
-
+        Box::new(fut)
     }
 
     pub fn display(state: State) -> (State, Response) {
