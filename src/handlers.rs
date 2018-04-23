893dd00a86c6e746aba255c9e4b4440e9269e3d8
Rrogntudju <mario.prince@outlook.com>
2018-04-23 15:03:37 -0400

    display

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,7 +4,7 @@ pub mod api {
     use gotham::http::response::create_response;
     use gotham::state::{State, FromState};
     use gotham::handler::HandlerFuture;
-    use hyper::{Body, Chunk, Error, Response, StatusCode};
+    use hyper::{Body, Chunk, Error, StatusCode};
     use mime;
     use futures::{Future, future, Stream};
     use serde_json;
@@ -17,20 +17,18 @@ pub mod api {
     }
     
     #[derive(Serialize)]
-    struct SudokuResponse<T> {
+    struct SolveResponse {
         status: String,
-        data: T
+        data: String,
+        message: String
     }    
 
-    #[derive(Serialize)]
-    struct DisplayData {
-        display: Vec<String>
-    }
-
-    #[derive(Serialize)]
-    struct SolutionData {
-        solution: String
-    }
+     #[derive(Serialize)]
+    struct DisplayResponse {
+        status: String,
+        data: Vec<String>,
+        message: String
+    }    
 
     fn solve_sudoku(body: Result<Chunk, Error>) -> impl Future<Item=String, Error=PuzzleError> {
         match body {
@@ -42,8 +40,29 @@ pub mod api {
                     Ok(sp) => {
                         let solution = Sudoku::new().solve(&sp.puzzle);
                         match solution {
-                            Ok(s) => future::ok(s),
-                            Err(e) => future::err(e)
+                            Ok(s)   => future::ok(s),
+                            Err(e)  => future::err(e)
+                        }
+                    }
+                    _ => future::err(PuzzleError::InvalidGrid)
+                }
+            }
+            _ => future::err(PuzzleError::InvalidGrid)
+        }
+    }
+
+    fn display_sudoku(body: Result<Chunk, Error>) -> impl Future<Item=Vec<String>, Error=PuzzleError> {
+        match body {
+            Ok(valid_body) => {
+                let body_content = String::from_utf8(valid_body.to_vec()).unwrap();
+                println!("Body: {}", body_content);
+                let sudoku_puzzle: Result<SudokuRequest, _> = serde_json::from_str(&body_content);
+                match sudoku_puzzle {
+                    Ok(sp) => {
+                        let grid = Sudoku::display(&sp.puzzle);
+                        match grid {
+                            Ok(s)   => future::ok(s),
+                            Err(e)  => future::err(e)
                         }
                     }
                     _ => future::err(PuzzleError::InvalidGrid)
@@ -61,13 +80,15 @@ pub mod api {
             .then(|solve_result| { 
                 let sudoku_response = 
                     match solve_result {
-                        Ok(solution) => SudokuResponse {status: "success".into(), data: SolutionData {solution: solution}},
-                        Err(e) => SudokuResponse {status: "fail".into(), data: SolutionData {solution: format!("{}", e)}},
+                        Ok(solution)    => SolveResponse {status: "success".into(), data: solution, message: "".into()},
+                        Err(e)          => SolveResponse {status: "fail".into(), data: "".into(), message: format!("{}", e)}
                     };
+                let json_response = serde_json::to_string(&sudoku_response).unwrap();
+                println!("JSON: {}", json_response);
                 let resp = create_response(
                     &state,
                     StatusCode::Ok,
-                    Some((serde_json::to_string(&sudoku_response).unwrap().into_bytes(), mime::APPLICATION_JSON))
+                    Some((json_response.into_bytes(), mime::APPLICATION_JSON))
                 );
                 future::ok((state, resp))
             }
@@ -76,13 +97,29 @@ pub mod api {
         Box::new(fut)
     }
 
-    pub fn display(state: State) -> (State, Response) {
-        let res = create_response(
-            &state,
-            StatusCode::Ok,
-            Some((String::from(stringify!($t)).into_bytes(), mime::TEXT_PLAIN)),
+    pub fn display(mut state: State) -> Box<HandlerFuture> {
+        let fut = 
+            Body::take_from(&mut state)
+            .concat2()
+            .then(display_sudoku)
+            .then(|grid_result| { 
+                let sudoku_response = 
+                    match grid_result {
+                        Ok(grid)    => DisplayResponse {status: "success".into(), data: grid, message: "".into()},
+                        Err(e)      => DisplayResponse {status: "fail".into(), data: Vec::new(), message: format!("{}", e)}
+                    };
+                let json_response = serde_json::to_string(&sudoku_response).unwrap();
+                println!("JSON: {}", json_response);
+                let resp = create_response(
+                    &state,
+                    StatusCode::Ok,
+                    Some((json_response.into_bytes(), mime::APPLICATION_JSON))
+                );
+                future::ok((state, resp))
+            }
         );
 
-    (state, res)
+        Box::new(fut)
     }
+
 }
