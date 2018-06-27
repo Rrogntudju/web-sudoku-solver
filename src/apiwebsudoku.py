a898520ddcfd6f00ed057057c0323c0bc7adc4c2
Rrogntudju <mario.prince@outlook.com>
2018-06-27 18:14:17 -0400

    generators instead of lists

--- src/apiwebsudoku.py
+++ src/apiwebsudoku.py
@@ -14,8 +14,8 @@ puzzle_link = soup("frame")[0]["src"]
 puzzle_page = http.request("get", puzzle_link)
 soup = BeautifulSoup(puzzle_page.data, "lxml")
 puzzle_values = {tag["id"] : tag["value"] for tag in soup("input", readonly=True)}
-ids = ["f" + str(j) + str(i) for i in range(9) for j in range(9)]
-puzzle = "".join([puzzle_values[id] if id in puzzle_values else "0" for id in ids])
+ids = ("f" + str(j) + str(i) for i in range(9) for j in range(9))
+puzzle = "".join(puzzle_values[id] if id in puzzle_values else "0" for id in ids)
 
 # Solve with sudoku web api
 api_url = "http://localhost:7878/api"
